cdcac4e47d569cc7deabb3987bf8b96ba66a4532
Tyler Radke <radketyler1@gmail.com>
2022-09-16 10:41:53 -0600

    About me project

---
+++ About Me/About Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Tyler Radke on 9/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
