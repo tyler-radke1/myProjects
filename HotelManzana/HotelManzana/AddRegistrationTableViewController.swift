1d71209473821da196274e76075e6a55f6a426cc
Tyler Radke <radketyler1@gmail.com>
2022-10-26 10:06:42 -0600

    show hide date pickers

--- HotelManzana/HotelManzana/AddRegistrationTableViewController.swift
+++ HotelManzana/HotelManzana/AddRegistrationTableViewController.swift
@@ -15,9 +15,38 @@ class AddRegistrationTableViewController: UITableViewController {
     
     @IBOutlet weak var emailTextField: UITextField!
     
+    @IBOutlet weak var checkInDateLabel: UILabel!
+    
+    @IBOutlet weak var checkInDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var checkOutDateLabel: UILabel!
+    
+    @IBOutlet weak var checkOutDatePicker: UIDatePicker!
+    
+    let checkInDatePickerIndexPath = IndexPath(row: 1, section: 1)
+    let checkInDateLabelIndexPath = IndexPath(row: 0, section: 1)
+    
+    let checkOutDatePickerIndexPath = IndexPath(row: 3, section: 1)
+    let checkOutDateLabelIndexPath = IndexPath(row: 2, section: 1)
+    
+    var isCheckInDatePickerVisible: Bool = false {
+        didSet {
+            checkInDatePicker.isHidden = !isCheckInDatePickerVisible
+        }
+    }
+    
+    var isCheckOutDatePickerVisible: Bool = false {
+        didSet {
+            checkOutDatePicker.isHidden = !isCheckOutDatePickerVisible
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateDateViews()
+        let midnight = Calendar.current.startOfDay(for: Date())
+        checkInDatePicker.minimumDate = midnight
+        checkInDatePicker.date = midnight
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -34,7 +63,67 @@ class AddRegistrationTableViewController: UITableViewController {
         let email = emailTextField.text
         
         print("First Name \(firstName) Last Name: \(lastName) Email: \(email)")
+        
+        print("Checkin \(checkOutDatePicker.date), checkout \(checkOutDatePicker.date)")
+    }
+    
+    func updateDateViews() {
+        checkOutDatePicker.minimumDate = Calendar.current.date(byAdding: .day, value: 1, to: checkInDatePicker.date)
+        
+        checkInDateLabel.text = checkInDatePicker.date.formatted(date: .abbreviated, time: .omitted)
+        
+        checkOutDateLabel.text = checkOutDatePicker.date.formatted(date: .abbreviated, time: .omitted)
+        
     }
     
     
+    @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+        updateDateViews()
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case checkInDatePickerIndexPath where isCheckInDatePickerVisible == false: return 0
+        case checkOutDatePickerIndexPath where isCheckOutDatePickerVisible == false: return 0
+        default : return UITableView.automaticDimension
+        }
+        
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+            
+        case checkInDatePickerIndexPath:
+            return 216
+        case checkOutDatePickerIndexPath:
+            return 216
+        default:
+            return UITableView.automaticDimension
+            
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath == checkInDateLabelIndexPath && isCheckOutDatePickerVisible == false {
+            isCheckInDatePickerVisible.toggle()
+        } else if indexPath == checkOutDateLabelIndexPath && isCheckInDatePickerVisible == false {
+            isCheckOutDatePickerVisible.toggle()
+        } else if indexPath == checkInDateLabelIndexPath || indexPath == checkOutDateLabelIndexPath {
+            isCheckInDatePickerVisible.toggle()
+            isCheckOutDatePickerVisible.toggle()
+        } else {
+            return
+        }
+        
+        
+        
+        
+        
+        
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
 }
