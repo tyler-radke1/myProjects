dd268904be886c2b5332438e41a3e8a2fec1344b
Tyler Radke <tylerradke@Tylers-iMac.local>
2023-01-10 08:19:42 -0700

    Finished the unit conversion project for swift ui.

--- 100 Days of SwiftUI/Time Conversion/Time Conversion/ContentView.swift
+++ 100 Days of SwiftUI/Time Conversion/Time Conversion/ContentView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
    @State private var enteredNumber = 0
-    @State private var selectedTime = Times.day
+    @State private var convertFrom = Times.day
+    @State private var convertTo = Times.hour
     
     enum Times: String, CaseIterable {
         
@@ -19,6 +20,67 @@ struct ContentView: View {
         case second = "Second"
     }
     
+    func doConversion(amount: Int, convertFrom: Times, convertTo: Times) -> Int {
+        if convertFrom == Times.day {
+            switch convertTo {
+            case .day:
+                return amount
+            case .hour:
+                return amount * 24
+            case .minute:
+                return amount * 1440
+            case .second:
+                return amount * 86400
+            }
+        }
+        
+        if convertFrom == Times.hour {
+            switch convertTo {
+            case .day:
+                return amount / 24
+            case .hour:
+                return amount
+            case .minute:
+                return amount * 60
+            case .second:
+                return amount * 3600
+            }
+                
+                
+            }
+        if convertFrom == Times.minute {
+            switch convertTo {
+            case .day:
+                return amount / 1440
+            case .hour:
+                return amount / 60
+            case .minute:
+                return amount
+            case .second:
+                return amount * 60
+            }
+        }
+            
+        if convertFrom == Times.second {
+            switch convertTo {
+            case .day:
+                return amount / 86440
+            case .hour:
+                return amount / 3600
+            case .minute:
+                return amount / 60
+            case .second:
+                return amount
+            
+            }
+        }
+        
+            return 0
+        }
+    
+
+    
+        
     
     var body: some View {
         
@@ -29,23 +91,21 @@ struct ContentView: View {
                 TextField("Amount", value: $enteredNumber, format: .number)
                     .keyboardType(.numberPad)
                 
-                
-                Picker("Time Unit", selection: $selectedTime) {
+                Picker("Time Unit", selection: $convertFrom) {
                     ForEach(Times.allCases, id: \.self) {
                         Text($0.rawValue)
                     }
                     
                 }
                 
-                
             } header: {
                 Text("Input")
             }
             
             Section {
-                Text("Text to be entered later")
+                Text(String(doConversion(amount: enteredNumber, convertFrom: convertFrom, convertTo: convertTo)))
                 
-                Picker("Time Unit", selection: $selectedTime) {
+                Picker("Time Unit", selection: $convertTo) {
                     ForEach(Times.allCases, id: \.self) {
                         Text($0.rawValue)
                     }
