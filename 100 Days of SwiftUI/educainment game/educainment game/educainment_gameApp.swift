baf36ae164798eae0952a630f85773b754596f28
Tyler Radke <radketyler1@gmail.com>
2023-01-18 07:42:31 -0700

    Started Day 35 project

---
+++ 100 Days of SwiftUI/educainment game/educainment game/educainment_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  educainment_gameApp.swift
+//  educainment game
+//
+//  Created by Tyler Radke on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct educainment_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
